5dbe5bd8d2667c091931c840387ccbb12fcfea78
chorok917 <chorok917@gmail.com>
2024-06-18 14:45:48 +0900

    customerDTO 구현

---
+++ src/ver01/CustomerDTO.java
@@ -0,0 +1,11 @@
+package ver01;
+
+public class CustomerDTO {
+
+	private int customer_id;
+	private String customer_name;
+	private String phone;
+	private String state;
+	private int location_id;
+	
+}
