0b0752df7d705d37b1c0fd2eaf5aa87eaeef3bb4
ddg999 <slowman918@gmail.com>
2024-06-18 14:42:16 +0900

    로케이션리저베이션

---
+++ src/ver01/LocationDTO.java
@@ -0,0 +1,19 @@
+package ver01;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class LocationDTO {
+
+	private int locationId;
+	private String locationName;
+
+}
---
+++ src/ver01/ReservationDTO.java
@@ -0,0 +1,21 @@
+package ver01;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class ReservationDTO {
+
+	private int reservationId;
+	private Boolean reservationState;
+	private String reservationTime;
+	private int customerId;
+	private int restaurantId;
+}
