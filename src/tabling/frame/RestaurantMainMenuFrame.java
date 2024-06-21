144db223015c3b70623162be5db9d1b569580a56
chorok917 <chorok917@gmail.com>
2024-06-21 10:05:52 +0900

    회원정보수정창 이미지 추가

--- src/tabling/frame/RestaurantMainMenuFrame.java
+++ src/tabling/frame/RestaurantMainMenuFrame.java
@@ -1,12 +1,20 @@
 package tabling.frame;
 
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
 
 import tabling.dto.RestaurantDTO;
 
 public class RestaurantMainMenuFrame extends JFrame {
 
 	private RestaurantDTO restaurantDTO;
+	// TODO - 테이블로 리스트 띄우기 하는 중
+	private JTable table;
+	private JScrollPane scroll;
+	private JComboBox<String> filter;
 
 	public RestaurantMainMenuFrame(RestaurantDTO restaurantDTO) {
 		this.restaurantDTO = restaurantDTO;
