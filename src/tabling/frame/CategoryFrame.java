36ff2b3d57960dd825fbc6fe672ea9f2de519635
ning00060 <ning00060@gmail.com>
2024-06-21 12:13:42 +0900

    수정완료

--- src/tabling/frame/CategoryFrame.java
+++ src/tabling/frame/CategoryFrame.java
@@ -100,7 +100,6 @@ public class CategoryFrame extends JFrame implements MouseListener {
 	@Override
 	public void mousePressed(MouseEvent e) {
 		try {
-<<<<<<< HEAD
 			for (int i = Define.CATEGORY_ALL; i <= Define.CATEGORY_HOF; i++) {
 				if (e.getSource() == categoryImgs[Define.CATEGORY_ALL]) {
 					RestaurantDAO dao = new RestaurantDAO();
@@ -108,46 +107,7 @@ public class CategoryFrame extends JFrame implements MouseListener {
 					new RestaurantListFrame(list,customerDTO,RestaurantListFrame.CATEGORY_ALL);
 					setVisible(false);
 					break;
-=======
-
-			
-	for(int i=0;i<3;i++) {
-		 if (i==0) {			 
-			 if(e.getSource() == categoryImgs[i]  ) {
-				 
-					RestaurantDAO dao = new RestaurantDAO();
-					List<RestaurantDTO> list = dao.getAllRestaurants();
-					new RestaurantListFrame(list, customerDTO, RestaurantListFrame.CATEGORY);	
-			 }
-		} else {
-	 
-		  if (e.getSource() == categoryImgs[i]  ) {
-			RestaurantDAO dao = new RestaurantDAO();
-				List<RestaurantDTO> list = dao.getRestaurantsByCategory(i);
-				new RestaurantListFrame(list, customerDTO, RestaurantListFrame.CATEGORY);
-		  			}
-		 	}  
-		 
-		 
-		 if(e.getSource() == homeLable) {
-			 new CustomerMainMenuFrame(customerDTO);
-
-
-			for (int i = Define.CATEGORY_ALL; i < Define.CATEGORY_HOF; i++) {
-				if (e.getSource() == categoryImgs[i]) {
-					if (i == Define.CATEGORY_ALL) {
-						RestaurantDAO dao = new RestaurantDAO();
-						List<RestaurantDTO> list = dao.getAllRestaurants();
-						new RestaurantListFrame(list);
-						break;
-
-					} else {
-						RestaurantDAO dao = new RestaurantDAO();
-						List<RestaurantDTO> list = dao.getRestaurantsByCategory(i);
-						new RestaurantListFrame(list);
-						break;
-					}
->>>>>>> c5bd54d696df7125cbb6f79d9197eccf3052bb4f
+				
 				}
 				if (e.getSource() == categoryImgs[i]) {
 					RestaurantDAO dao = new RestaurantDAO();
@@ -180,8 +140,8 @@ public class CategoryFrame extends JFrame implements MouseListener {
 		} catch (SQLException e1) {
 			e1.printStackTrace();
 		}
-	}
-		}
+	
+		
 	}
 
 	private class BackgroundPanel extends JPanel {
