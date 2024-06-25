52aaf3e9a3d21a0e883a192ee6a2cd4436da8e2c
kyeonghooon <gibbon0510@gmail.com>
2024-06-25 14:07:15 +0900

    ip 주소 로컬호스트로 변경

--- src/tabling/Tabling.java
+++ src/tabling/Tabling.java
@@ -10,7 +10,7 @@ import tabling.request.Request;
 public class Tabling {
 
 	public static void main(String[] args) {
-		Request.setIp("192.168.0.129");
+		Request.setIp("localhost");
 		UIManager.put("OptionPane.messageFont", new Font("Noto Sans KR", Font.PLAIN, 12));
 		new LoginSelectFrame();
 	}
