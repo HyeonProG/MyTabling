d2a58fc5f2521c711f999001b400404dacc71623
kyeonghooon <gibbon0510@gmail.com>
2024-06-26 09:08:35 +0900

    최종 완료

--- src/tabling/Tabling.java
+++ src/tabling/Tabling.java
@@ -10,6 +10,7 @@ import tabling.request.Request;
 public class Tabling {
 
 	public static void main(String[] args) {
+		// 서버 주소 변경 가능
 		Request.setIp("localhost");
 		UIManager.put("OptionPane.messageFont", new Font("Noto Sans KR", Font.PLAIN, 12));
 		new LoginSelectFrame();
